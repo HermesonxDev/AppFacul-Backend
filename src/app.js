adde16c7f1748e6e3708454760819ef036afbdbf
HermesonxBerp <hermesonsantos@berp.com.br>
2024-11-24 09:19:38 -0300

    fixes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
+const cors = require("cors")
 
-app.use(express.json())
+const app = express()
 
 const userRoutes = require('./routes/userRoutes')
 
+app.use(express.json())
+app.use(cors())
 app.use('/users', userRoutes)
 
 app.use((err, req, res, next) => {
